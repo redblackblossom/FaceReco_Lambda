c2f747677f4e3ccab468f3fc1c86dde44832a6fc
redblackblossom <155342235+redblackblossom@users.noreply.github.com>
2024-03-15 22:39:21 +0900

    complete aws lambda test

--- imageCaption/imageCaption.py
+++ imageCaption/imageCaption.py
@@ -4,21 +4,27 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import boto3
 import io
-BUCKET_NAME = "face-recognition-image-caption"
-
-s3 = boto3.client("s3")
+import os
 
 def handler(event, context):
+    os.environ["TRANSFORMERS_CACHE"] = "/tmp"
+    BUCKET_NAME = "face-recognition-image-caption"
+    s3 = boto3.client("s3")
     #모델 로드
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+    model_path = os.path.join(os.environ['LAMBDA_TASK_ROOT'], "models")
+    processor = BlipProcessor.from_pretrained(model_path)
+    model = BlipForConditionalGeneration.from_pretrained(model_path)
     #API 데이터 로드
-    body = json.loads(event['body'])
+    if isinstance(event['body'], (str, bytes, bytearray)):
+        body = json.loads(event['body'])
+    else:
+        body = event['body']
     imageDir = body["imageDir"]
     imageId = body["imageId"]
     #이미지 처리
-    image = s3.get_object(Bucket = BUCKET_NAME, Key = imageDir)
-    raw_image = Image.open(io.BytesIO(image)).convert('RGB')
+    image_response  = s3.get_object(Bucket = BUCKET_NAME, Key = imageDir)
+    image_data = image_response['Body'].read()
+    raw_image = Image.open(io.BytesIO(image_data)).convert('RGB')
     #모델에 이미지 넣음
     inputs = processor(raw_image, return_tensors="pt")
     out = model.generate(**inputs)
--- imageCaption/modelDownload.py
+++
@@ -1,4 +0,0 @@
-from transformers import BlipProcessor, BlipForConditionalGeneration
-
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
