0fa04925d684be57f6253389b24a5aa9f4721aef
redblackblossom <155342235+redblackblossom@users.noreply.github.com>
2024-04-02 15:22:48 +0900

    image caption logic was triggered by s3 change

--- imageCaption/imageCaption.py
+++ imageCaption/imageCaption.py
@@ -6,36 +6,32 @@ import boto3
 import io
 import os
 
+import esQuery
+
 def handler(event, context):
     os.environ["TRANSFORMERS_CACHE"] = "/tmp"
-    BUCKET_NAME = "face-recognition-image-caption"
     s3 = boto3.client("s3")
+    record = event['Records'][0]
+    bucket_name = record['s3']['bucket']['name'] 
+    object_key = record['s3']['object']['key']
+    print("object_key : ", object_key)
+    print("Bucket_name  : ", bucket_name)
+    #이미지 처리
+    image_response  = s3.get_object(Bucket = bucket_name, Key = object_key)
+    print("complete to load image from s3")
+    image_data = image_response['Body'].read()
+    raw_image = Image.open(io.BytesIO(image_data)).convert('RGB')
     #모델 로드
     model_path = os.path.join(os.environ['LAMBDA_TASK_ROOT'], "models")
     processor = BlipProcessor.from_pretrained(model_path)
     model = BlipForConditionalGeneration.from_pretrained(model_path)
-    #API 데이터 로드
-    if isinstance(event['body'], (str, bytes, bytearray)):
-        body = json.loads(event['body'])
-    else:
-        body = event['body']
-    imageDir = body["imageDir"]
-    imageId = body["imageId"]
-    #이미지 처리
-    image_response  = s3.get_object(Bucket = BUCKET_NAME, Key = imageDir)
-    image_data = image_response['Body'].read()
-    raw_image = Image.open(io.BytesIO(image_data)).convert('RGB')
+    print("complete to load model")
     #모델에 이미지 넣음
     inputs = processor(raw_image, return_tensors="pt")
     out = model.generate(**inputs)
     imageCaptionResult = processor.decode(out[0], skip_special_tokens=True)
-
-    resultApi = {
-        "imageId" : imageId,
-        "caption" : imageCaptionResult
-    }
-
-    return{
-        "statusCode": 200,
-        'body': json.dumps(resultApi)
-    }
+    print("imageCaption : " , imageCaptionResult)
+    #api trigger
+    awsName = object_key.split("/")[-1]
+    print("awsName : ", awsName)
+    esQuery.elasticSearchApi(awsName, imageCaptionResult)
